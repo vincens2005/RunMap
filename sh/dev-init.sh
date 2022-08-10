2236f0dfe36701f5dd9f6a35ede6c57f0baec06a
cukmekerb <cukmekerb@gmail.com>
2022-08-09 21:00:51 -0700

    remove google analytics and stuff

--- sh/dev-init.sh
+++ sh/dev-init.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-encoded=$(printf '%s' $1 | base64)
 secret_path="./src/appsettings.secrets.ts"
 echo "export const ps = '$encoded';" > $secret_path
 echo "Base64 encoded key written to $secret_path"
+
